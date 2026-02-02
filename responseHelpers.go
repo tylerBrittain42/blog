66f2fc3f45ce421ad7df5e31c8fe900fc03b1227
Tyler Brittain <tbrittain100@gmail.com>
2026-02-01 20:55:24 -0800

    use error template

--- responseHelpers.go
+++ responseHelpers.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func respondWithError(w http.ResponseWriter, msg string, status int) {
 	w.WriteHeader(status)
-	_, err := w.Write([]byte(msg))
+	t, err := template.ParseFiles("template/error.html")
 	if err != nil {
-		log.Printf("error: %v\n", err)
+		log.Printf("template parse error: %v\n", err)
+		http.Error(w, msg, status)
+		return
 	}
 
+	data := map[string]interface{}{
+		"Status":       status,
+		"ErrorMessage": msg,
+	}
+
+	if err := t.Execute(w, data); err != nil {
+		log.Printf("template execute error: %v\n", err)
+	}
 }
